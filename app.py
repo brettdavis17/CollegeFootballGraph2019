8edb3ede533164c399c9a3f975d46453b067c5c0
brettdavis <brettdavis17@gmail.com>
2020-06-14 11:13:07 -0500

    add import and data directories

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import os
 import json
 from py2neo import Graph
+import pandas as pd
 
+
+# Confidential authentication details
 scheme = os.environ.get('SCHEME')
 host = os.environ.get('HOST')
 user = os.environ.get('USER')
 password = os.environ.get('PASSWORD')
 
+#Connect to graph
 graph = Graph(
     scheme=scheme,
     host=host,
@@ -18,6 +22,7 @@ graph = Graph(
     )
 )
 
+#Access data files for load
 with open('data/conferences.json') as conferences_file:
     conferences = json.load(conferences_file)
 
@@ -33,6 +38,71 @@ with open('data/drives.json') as drives_file:
 with open('data/venues.json') as venues_file:
     venues = json.load(venues_file)
 
+with open('data/plays/week1.json') as week_1_file:
+    week_1_plays = json.load(week_1_file)
+
+with open('data/plays/week2.json') as week_2_file:
+    week_2_plays = json.load(week_2_file)
+
+with open('data/plays/week3.json') as week_3_file:
+    week_3_plays = json.load(week_3_file)
+
+with open('data/plays/week4.json') as week_4_file:
+    week_4_plays = json.load(week_4_file)
+
+with open('data/plays/week5.json') as week_5_file:
+    week_5_plays = json.load(week_5_file)
+
+with open('data/plays/week6.json') as week_6_file:
+    week_6_plays = json.load(week_6_file)
+
+with open('data/plays/week7.json') as week_7_file:
+    week_7_plays = json.load(week_7_file)
+
+with open('data/plays/week8.json') as week_8_file:
+    week_8_plays = json.load(week_8_file)
+
+with open('data/plays/week9.json') as week_9_file:
+    week_9_plays = json.load(week_9_file)
+
+with open('data/plays/week10.json') as week_10_file:
+    week_10_plays = json.load(week_10_file)
+
+with open('data/plays/week11.json') as week_11_file:
+    week_11_plays = json.load(week_11_file)
+
+with open('data/plays/week12.json') as week_12_file:
+    week_12_plays = json.load(week_12_file)
+
+with open('data/plays/week13.json') as week_13_file:
+    week_13_plays = json.load(week_13_file)
+
+with open('data/plays/week14.json') as week_14_file:
+    week_14_plays = json.load(week_14_file)
+
+with open('data/plays/week15.json') as week_15_file:
+    week_15_plays = json.load(week_15_file)
+
+#There are too many plays, so I'm loading them in weekly batches
+plays_list = [
+    week_1_plays,
+    week_2_plays,
+    week_3_plays,
+    week_4_plays,
+    week_5_plays,
+    week_6_plays,
+    week_7_plays,
+    week_8_plays,
+    week_9_plays,
+    week_10_plays,
+    week_11_plays,
+    week_12_plays,
+    week_13_plays,
+    week_14_plays,
+    week_15_plays
+]
+
+#varable for loading conference nodes
 conferences_cypher = '''
     WITH $json as data
     UNWIND data as c
@@ -42,6 +112,7 @@ conferences_cypher = '''
         conference.abbreviation = c.abbreviation
 '''
 
+#variable for loading team nodes
 teams_cypher = '''
     WITH $json as data
     UNWIND data as t
@@ -58,6 +129,7 @@ teams_cypher = '''
         team.altColor = t.alt_color
 '''
 
+#variable for loading venues
 venues_cypher = '''
     WITH $json as data
     UNWIND data as v
@@ -75,14 +147,15 @@ venues_cypher = '''
         venue.timezone = v.timezone
 '''
 
+#variable for loading games, season, and week.  Also establishes relationships.
 games_cypher = '''
     WITH $json as data
     UNWIND data as g
     MERGE (season:Season {seasonId: g.season})
     MERGE (week:Week {weekId: toString(g.season) + toString(g.week)})
     MERGE (game:Game {gameId: g.id, venueId: g.venue_id, homeTeam: g.home_team, awayTeam: g.away_team})
-    MERGE (Season)-[:HAS_WEEK]->(Week)
-    MERGE (Week)-[:HAS_GAME]->(Game)
+    MERGE (season)-[:HAS_WEEK]->(week)
+    MERGE (week)-[:HAS_GAME]->(game)
     MERGE (home:Team {school: g.home_team})
     MERGE (away:Team {school: g.away_team})
     MERGE (away)<-[rAway:HAS_PARTICIPANT_TEAM]-(game)-[rHome:HAS_PARTICIPANT_TEAM]->(home)
@@ -121,16 +194,11 @@ games_cypher = '''
         rHome.opponentScore = g.away_points
 '''
 
-home_stadium_cypher = '''
-    MATCH (venue:Venue)-[:HOSTED_GAME]->(game:Game)-[:HAS_PARTICIPANT_TEAM {homeOrAway: 'home'}]->(team:Team)
-    WHERE game.isNeutral = false
-    MERGE (team)-[:HAS_HOME_STADIUM]->(venue)
-'''
-
+#variable for loading drives and establish relationships
 drives_cypher = '''
     WITH $json as data
     UNWIND data as d
-    MERGE (drive:Drive {driveId: toInteger(d.id), gameId: d.game_id, offense: d.offense, defense: d.defense})
+    MERGE (drive:Drive {driveId: d.id, gameId: d.game_id, offense: d.offense, defense: d.defense})
     MERGE (offense:Team {school: d.offense})
     MERGE (defense:Team {school: d.defense})
     MERGE (game:Game {gameId: d.game_id})
@@ -150,6 +218,44 @@ drives_cypher = '''
         drive.driveResult = d.drive_result
 '''
 
+#variable for loading plays and establishing relationships
+plays_cypher = '''
+    WITH $json as data
+    UNWIND data as p
+    MERGE (play:Play {
+        playId:p.id,
+        offense:p.offense,
+        defense:p.defense,
+        index:p.index,
+        driveId:p.drive_id
+        }
+    )
+    MERGE (offense:Team {school:p.offense})
+    MERGE (defense:Team {school:p.defense})
+    MERGE (drive:Drive {driveId:p.drive_id})
+    MERGE (drive)-[:HAS_PLAY]->(play)
+    MERGE (offense)-[:ON_OFFENSE_PLAY]->(play)<-[:ON_DEFENSE_PLAY]-(defense)
+    SET play.offenseConference = p.offense_conference,
+        play.offenseScore = p.offense_score,
+        play.defenseConference = p.defense_conference,
+        play.defenseScore = p.defense_score,
+        play.home = p.home,
+        play.away = p.away,
+        play.period = p.period,
+        play.clockSec = p.clockSec,
+        play.clockDisplay = p.clockDisplay,
+        play.gameTimeLeftSec = p.gameTimeLeftSec,
+        play.yardLine = p.yard_line,
+        play.yardsToGoal = p.yards_to_goal,
+        play.down = p.down,
+        play.distance = p.distance,
+        play.yardsGained = p.yards_gained,
+        play.playType = p.play_type,
+        play.playText = p.play_text,
+        play.ppa = p.ppa
+'''
+
+#variable establishing relationships between teams and their conference
 teams_conferences_cypher = '''
     MATCH (conference:Conference)
     MATCH (team:Team)
@@ -157,6 +263,7 @@ teams_conferences_cypher = '''
     MERGE (conference)-[:HAS_MEMBER_SCHOOL]->(team)
 '''
 
+#variable establishing relationship between a game and the venue hosting it
 game_venue_cypher = '''
     MATCH (venue:Venue)
     MATCH (game:Game)
@@ -164,18 +271,52 @@ game_venue_cypher = '''
     MERGE (venue)-[:HOSTED_GAME]->(game)
 '''
 
+#variable establishing relationship between a team and the venue it uses as home stadium
+home_stadium_cypher = '''
+    MATCH (venue:Venue)-[:HOSTED_GAME]->(game:Game)-[r:HAS_PARTICIPANT_TEAM]->(team:Team)
+    WHERE game.neutralSite = false AND r.homeOrAway = 'home'
+    MERGE (team)-[:HAS_HOME_STADIUM]->(venue)
+'''
+
+#variable establishing relationship of plays in sequence in a drive
+plays_index_cypher = '''
+    MATCH (play1:Play)
+    MATCH (play2:Play)
+    WHERE play1.index = play2.index - 1 AND play1.driveId = play2.driveId
+    MERGE (play1)-[:FOLLOWED_BY_PLAY]->(play2)
+'''
+
+#load conferences with conferences cypher variable
 graph.run(conferences_cypher, json=conferences)
 
+#load teams with teams cypher variable
 graph.run(teams_cypher, json=teams)
 
+#load venues with venues cypher variable
 graph.run(venues_cypher, json=venues)
 
+#load games with games cypher variable
 graph.run(games_cypher, json=games)
 
+#load drives with drives cypher variable
 graph.run(drives_cypher, json=drives)
 
+#load teams to conferences relationship with cypher variable
 graph.run(teams_conferences_cypher)
 
+#load games to venue relationship with cypher variable
+graph.run(game_venue_cypher)
+
+#load teams to home stadium relationship with cypher variable
 graph.run(home_stadium_cypher)
 
-graph.run(game_venue_cypher)
+#create for loop to execute batch loads of plays
+for p in plays_list:
+
+    graph.run(
+        plays_cypher,
+        json=p
+    )
+
+#load followed-by-play relationships with cypher variable
+graph.run(plays_index_cypher)
